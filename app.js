ae36c79a87632aea6b575cf7d6c8483f333395de
IT21023378 <it21023378@my.sliit.lk>
2021-12-30 23:17:42 +0530

    aded details page

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 
 let movieImagesLink = [];
 let searchTitle = "";
+let movieId = [];
+let clickedID = "";
+let actorName = [];
+let actorcharacter = [];
+let actorImg = [];
 
 app.set("view engine", "ejs");
 
@@ -20,20 +25,27 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   searchTitle = req.body.search;
   searchTitle = searchTitle.charAt(0).toUpperCase() + searchTitle.slice(1);
+  res.redirect("/result");
+});
+
+app.get("/result", (req, res) => {
   const url =
-    "https://imdb-api.com/en/API/SearchMovie/k_5m8lsk5x/" + searchTitle;
+    "https://imdb-api.com/en/API/SearchMovie/k_x5ev0pv7/" + searchTitle;
 
   axios
-    .request(url)
+    .get(url)
     .then(function (response) {
       movieImagesLink = [];
+      movieId = [];
       for (let i = 0; i < response.data.results.length; i++) {
         movieImagesLink.push(response.data.results[i].image);
+        movieId.push(response.data.results[i].id);
       }
 
       res.render("result", {
         movieImages: movieImagesLink,
         title: searchTitle,
+        movieId: movieId,
       });
     })
     .catch(function (error) {
@@ -41,8 +53,60 @@ app.post("/", (req, res) => {
     });
 });
 
+app.post("/result", (req, res) => {
+  clickedID = req.body.submit;
+  console.log(clickedID);
+  res.redirect("/details");
+});
+
 app.get("/details", (req, res) => {
-  res.render("details");
+  const url1 =
+    "https://imdb-api.com/en/API/Title/k_x5ev0pv7/" +
+    clickedID +
+    "/Trailer,Ratings,Wikipedia";
+
+  axios
+    .get(url1)
+    .then(function (response) {
+      let image = response.data.image;
+      let name = response.data.title;
+      let desc = response.data.plot;
+      let genre = response.data.genres;
+      let lan = response.data.languages;
+      let director = response.data.directors;
+      let year = response.data.year;
+      let imdb = response.data.imDbRating;
+      let rotten = response.data.ratings.rottenTomatoes;
+      let plot = response.data.wikipedia.plotFull.plainText;
+      actorName = [];
+      actorcharacter = [];
+      actorImg = [];
+
+      for (var i = 0; i < response.data.actorList.length; i++) {
+        actorName.push(response.data.actorList[i].name);
+        actorcharacter.push(response.data.actorList[i].asCharacter);
+        actorImg.push(response.data.actorList[i].image);
+      }
+
+      res.render("details", {
+        image: image,
+        name: name,
+        desc: desc,
+        genre: genre,
+        director: director,
+        lan: lan,
+        year: year,
+        imdb: imdb,
+        rotten: rotten,
+        plot: plot,
+        castname: actorName,
+        castimg: actorImg,
+        castcar: actorcharacter,
+      });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
